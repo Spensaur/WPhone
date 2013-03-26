eae01cb8c069263fc23b192a8a9ad5152bae9a65
Spensaur <sir.spensaur@gmail.com>
2013-03-25 20:05:18 -0700

    XML Test

---
+++ XMLTest/XMLTest/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace XMLTest
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBlock_Tap(object sender, GestureEventArgs e)
+        {
+            Class1 test = new Class1();
+            test.getData("http://horstmann.com/sjsu/spring2013/cs185c/hw02/quiz.xml", textBlock);
+            //textBlock.Text = text;
+
+        }
+
+        public void setTextBlockText(String s)
+        {
+            textBlock.Text = s;
+        }
+    }
+}
